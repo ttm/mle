d19eb980ba2c7698eb390e5d78f56fbf4cee99cf
Renato Fabbri <renato.fabbri@gmail.com>
2018-07-14 21:07:49 -0300

    fazendo implementacoes iniciais ml

--- multilevel/basic.py
+++ multilevel/basic.py
@@ -21,3 +21,57 @@ class MLS:
     def _collapse(self):
         pass
 
+class MLS1:
+    """
+    Multilevel Strategy class
+
+    """
+    def __init__(self):
+        self.gs = []
+        self.npos = {}
+        self.status = ''
+    def setNetwork(self, network):
+        self.g = network
+        self.gs.append(self.g)
+        self.nodes = tuple(self.g.nodes())
+        self.nodes_ = set(self.nodes)
+        self.status += 'networkset|'
+    def mkMetaNetwork(self, level=-1, method='mod'):
+        assert 'networkset' in self.status
+        if level == -1:
+            level = len(self.npos)
+        if method == 'cp':
+            per = set(x.periphery(g))
+            cen = set(x.center(g))
+            pc = per.union(cen)
+            nodes = set(g.nodes())
+            inter = nodes.difference(pc)
+            gg = x.Graph()
+            gg.add_node(0, label='center', weight=len(cen), children=cen)
+            gg.add_node(1, label='intermediary', weight=len(inter), children=inter)
+            gg.add_node(2, label='periphery', weight=len(per), children=per)
+            # contar as arestas entre os membros dos supervertices
+            # ou montar lista com os vizinhos, depois contar quantas vezes cada membro ocorreu
+            # ou montar dicionario em que o vertice eh chave e o sv eh o valor, ao iterar pelos vizinhos, jah considerar o sv correspondente
+    def collapse(self):
+        for snode in self.snodes[-1]:
+            # calculate centroid
+            # give size in proportion to nchilds
+            # make dictionary where each node is key and the snode is the value
+            #   will be used for obtaining the super-edges
+    def mkLayout(self, level=0, inherit=1):
+        if inherit = 0:
+            self.mkRawLayout(level)
+        else:
+            if level == 0:
+                self.mkRawLayout(level)
+            else:
+                # put vertices in the centroid of its children
+                pass
+    def mkRawLayout(self, level):
+        l = self.layouts[self.layout](self.gs[level], dim=self.dim)
+        nodepos = n.array([l[i] for i in self.nodes])
+        # edges = [list(i) for i in g.edges]
+        # nodepos = (2*n.random.random((nn,3))-1).tolist()
+        # self.llayouts[level] = (nodepos, edges))
+        self.npos[level] = nodepos
--- server/first.py
+++ server/first.py
@@ -40,7 +40,7 @@ nets = [
         '/home/renato/Dropbox/Public/doc/vaquinha/FASE1/aa.gml',
         '/home/renato/Dropbox/Public/doc/vaquinha/FASE1/automata18022013.gml',
         '/home/renato/Dropbox/Public/doc/vaquinha/FASE1/GabiThume_huge_100002011676407_2013_02_19_04_40_b60204320447f88759e1efe7b031357b.gml',
-        '/home/renato/Dropbox/Public/doc/vaquinha/FASE1/LarissaAnzoateguihuge_1760577842_2013_02_20_02_07_f297e5c8675b72e87da409b2629dedb3.gml'
+        '/home/renato/Dropbox/Public/doc/vaquinha/FASE1/LarissaAnzoateguihuge_1760577842_2013_02_20_02_07_f297e5c8675b72e87da409b2629dedb3.gml',
         '/home/renato/Dropbox/Public/doc/vaquinha/FASE1/PedroRochaAttaktorZeros10032013.gml',
        ]
 layouts = {
@@ -64,3 +64,27 @@ def net(net, layout, dim=3):
     edges = [list(i) for i in g.edges]
     # nodepos = (2*n.random.random((nn,3))-1).tolist()
     return jsonify({'nodes': nodepos, 'edges': edges})
+
+@app.route("/level/<int:net>/<layout>/<int:dim>/<int:links>/<int:level>/<method>/")
+def level_(net, layout, dim=3, links=1, level=1, method='mod'):
+    # modularity, min_cut, center-periphery, hubs-int-per, 
+    if level == 0:
+        return render_template('basicURLInterface.html', net=net, layout=layout, dim=dim, links=links)
+    if method == 'cp':  # center-periphery
+        if level == 2:
+            nodepos = [[0, 0, 0]]
+            edges = []
+        if level == 1:
+            g = ml.parsers.GMLParser(nets[net]).g
+            per = set(x.periphery(g))
+            cen = set(x.center(g))
+            pc = per.union(cen)
+            nodes = set(g.nodes())
+            inter = nodes.difference(pc)
+            # l = layouts['spring'](g_, dim=dim)
+            nodepos = [(-1,0,0,len(per)), (0,0,0,len(inter)), (1,0,0,len(cen))]
+            edges = [list(i) for i in g.edges]
+    return jsonify({'nodes': nodepos, 'edges': edges})
+
+    return render_template('basicURLMLInterface.html', net=net, layout=layout, dim=dim, links=links, ml=ml, level=level)
+
