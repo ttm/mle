526f78dda0eb7f491867161aba71f349ee50cfd2
Renato Fabbri <renato.fabbri@gmail.com>
2018-08-06 12:56:55 -0300

    arquivo de utils, havia esquecido de add

---
+++ multilevel/utils.py
@@ -0,0 +1,7 @@
+import os
+fpath = os.path.dirname(os.path.abspath(__file__))
+def absoluteFilePaths(directory):
+   for dirpath,_,filenames in os.walk(directory):
+       for f in filenames:
+           yield os.path.abspath(os.path.join(dirpath, f))
+
