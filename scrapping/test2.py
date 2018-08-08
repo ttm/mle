d58979c70d1db0b79397f539e5827a5f5b17b468
Renato Fabbri <renato.fabbri@gmail.com>
2018-08-08 10:57:28 -0300

    feito disparador do scrapper

--- scrapping/test2.py
+++ scrapping/test2.py
@@ -1,11 +1,14 @@
 from splinter import Browser
+# create a credentials.py file in this folder where you define your
+# email e password as strings
+from credentials import email, password
 
 browser = Browser('firefox')
 # Visit URL
 url = "http://www.facebook.com"
 browser.visit(url)
-browser.fill('email', 'greenkobold@gmail.com')
-browser.fill('pass', 'Jockey67')
+browser.fill('email', email)
+browser.fill('pass', password)
 # # Find and click the 'search' button
 # button = browser.find_by_name('btnK')
 button = browser.find_by_value('Log In')
