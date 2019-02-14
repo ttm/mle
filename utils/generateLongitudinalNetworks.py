5b51576349691e2d439afc7db06afbc07eca551a
Renato Fabbri <renato.fabbri@gmail.com>
2019-02-14 01:39:45 -0200

    made a minimal synthesizer of longitudinal networks in a minimal format

---
+++ utils/generateLongitudinalNetworks.py
@@ -0,0 +1,15 @@
+# this script is dedicated to generating (dummy) evolving networks
+# to aid development
+import numpy as n
+
+
+def eNet(nnodes=100, nlinks=1000):
+    edges_ = n.random.randint(0, nnodes, nlinks*2)
+    edges = n.reshape( edges_, (nlinks, 2) )
+    return edges
+
+def wEvNet(nnodes, nlinks, fname):
+    edges = list(eNet(nnodes, nlinks))
+    edges_ = '\n'.join(['%s %s' % (e[0], e[1]) for e in edges])
+    with open(fname, 'w') as f:
+        f.write(edges_)
