1b14e85a858849ecce6701bf0d9265c2387d7f13
Renato Fabbri <renato.fabbri@gmail.com>
2019-08-02 05:38:01 -0300

    timing routines

--- server/first.py
+++ server/first.py
@@ -1017,8 +1017,10 @@ def communicabilityNets():
             A = n.loadtxt(StringIO(net['data']), delimiter=',')
         else:
             A = n.loadtxt(StringIO(net['data']))
+        if A.shape[0] != A.shape[1]:
+            continue
         nnodes = A.shape[0]
-        nlinks = (n.maximum(A, A.T) - n.diag(n.diag(A))).sum() / 2
+        nlinks = n.count_nonzero(n.maximum(A, A.T) - n.diag(n.diag(A))) / 2
         nets.append( {
             'filename': net['filename'],
             'nnodes': nnodes,
@@ -1347,3 +1349,51 @@ def saveSound():
     with open(fname_, 'wb') as f:
         f.write(blob)
     return 'ok'
+
+# https://stackoverflow.com/questions/24829726/python-function-to-return-javascript-date-gettime
+import time, datetime
+
+def now_milliseconds():
+   return int(time.time() * 1000)
+
+# reference time.time
+# Return the current time in seconds since the Epoch.
+# Fractions of a second may be present if the system clock provides them.
+# Note: if your system clock provides fractions of a second you can end up
+# with results like: 1405821684785.2
+# our conversion to an int prevents this
+
+def date_time_milliseconds(date_time_obj):
+   return int(time.mktime(date_time_obj.timetuple()) * 1000)
+
+def date_milliseconds():
+    return date_time_milliseconds(datetime.datetime.utcnow())
+
+# reference: time.mktime() will
+# Convert a time tuple in local time to seconds since the Epoch.
+
+msnowpyserver = now_milliseconds()
+
+msdatepyserver = date_milliseconds()
+
+@app.route("/timeStart/", methods=['POST'])
+def timestart():
+    mcalltimestart = now_milliseconds()
+    r = request.get_json()
+    return jsonify({
+            'msnowpyserver': msnowpyserver,
+            'msdatepyserver': msdatepyserver,
+            'mcall_sentfromserver': [
+                now_milliseconds(),
+                now_milliseconds(),
+                date_milliseconds(),
+                date_milliseconds(),
+                ],
+            'mcall_sentfromclient': r['requestTime']
+    })
+
+
+
+
+
+
